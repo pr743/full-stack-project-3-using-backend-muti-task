d8da244455b0fdef4884e577f3386a1ccf47bc22
Prince <princemungra1340@gmail.com>
2026-04-04 18:13:06 +0530

    update page

--- index.js
+++ index.js
@@ -30,11 +30,12 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  Credential: true
-}));
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 connectDB();
 
 app.get("/", (req, res) => {
--- socket/socket.js
+++ socket/socket.js
@@ -5,17 +5,17 @@ let io;
 export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "*",
+      origin: process.env.FRONTEND_URL,
+      methods: ["GET", "POST"],
+      credentials: true,
     },
   });
 
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
-
-    socket.on("join", (taskId) => {
+    socket.on("joinTask", (taskId) => {
       socket.join(taskId);
-      console.log("User joined room:", taskId);
     });
 
     socket.on("disconnect", () => {
@@ -24,9 +24,4 @@ export const initSocket = (server) => {
   });
 };
 
-export const getIO = () => {
-  if (!io) {
-    throw new Error("Socket.io not initialized");
-  }
-  return io;
-};
+export const getIO = () => io;
