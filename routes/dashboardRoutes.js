7e26094a13eba51b4804051b633aa4e80c36d156
Prince <princemungra1340@gmail.com>
2026-04-05 17:33:08 +0530

    update page

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getDashboardState } from "../controllers/dashboardController.js";
+import protect from "../middlewares/authMiddleware.js";
 
 
 const router = express.Router();
 
-router.get("/stats", getDashboardState);
+router.get("/stats", protect, getDashboardState);
 
 export default router;
 
